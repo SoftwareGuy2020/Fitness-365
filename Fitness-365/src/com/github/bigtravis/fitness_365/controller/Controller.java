d2d16865c1ab2c077ec9cf274798b219ee4dc787
Travis <twm.direct@gmail.com>
2018-04-23 03:27:52 -0700

    Added password hashing functionality, and preliminary views.

---
+++ Fitness-365/src/com/github/bigtravis/fitness_365/controller/Controller.java
@@ -0,0 +1,26 @@
+package com.github.bigtravis.fitness_365.controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Travis
+ *
+ */
+public class Controller extends Application {
+
+	/* (non-Javadoc)
+	 * @see javafx.application.Application#start(javafx.stage.Stage)
+	 */
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+
+	}
+
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
