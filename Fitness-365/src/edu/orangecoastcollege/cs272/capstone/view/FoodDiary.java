0048a2e838b427b26b2aac3ca3aaac2f36356f31
tmorrissey1 <tmorrissey1@OVDIMC10-126V.coast.cccd.adx>
2018-05-15 21:30:02 -0700

    updated macros

--- Fitness-365/src/edu/orangecoastcollege/cs272/capstone/view/FoodDiary.java
+++ Fitness-365/src/edu/orangecoastcollege/cs272/capstone/view/FoodDiary.java
@@ -7,6 +7,8 @@ import edu.orangecoastcollege.cs272.capstone.model.Category;
 import edu.orangecoastcollege.cs272.capstone.model.FoodDiaryEntry;
 import edu.orangecoastcollege.cs272.capstone.model.SceneNavigation;
 import javafx.beans.binding.IntegerExpression;
+import javafx.beans.property.DoubleProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
@@ -27,9 +29,9 @@ import jfxtras.scene.control.CalendarPicker;
 public class FoodDiary extends VBox implements SceneNavigation {
 
 	private static final String FXML_FILE_NAME = "food_diary.fxml";
-		
+
 	private Controller mController;
-	
+
 	@FXML
 	private PieChart macroPieChart;
 	@FXML
@@ -52,42 +54,69 @@ public class FoodDiary extends VBox implements SceneNavigation {
 	private Button addMealButton;
 	@FXML
 	private Button deleteMealButton;
-	
+
 	private ObservableList<FoodDiaryEntry> entries;
 
-	
-	public void initialize() {
-		entries = mController.getAllFoodDiaryEntries();		
-		breakfastTableView.setItems(entries.filtered(e -> e.getCategory() == Category.Breakfast));
-		lunchTableView.setItems(entries.filtered(e -> e.getCategory() == Category.Lunch));
-		dinnerTableView.setItems(entries.filtered(e -> e.getCategory() == Category.Dinner));
-		snacksTableView.setItems(entries.filtered(e -> e.getCategory() == Category.Snack));
-		
-		entries.addListener(new ListChangeListener<FoodDiaryEntry>() {
 
-			@Override
-			public void onChanged(Change<? extends FoodDiaryEntry> c) {
-				c.next();
-				if (c.wasAdded()) {
-					updateCalorieCounters(c.getAddedSubList().get(0).getMealCalories());
-				}				
-			}			
-		});
-		
-		int tdee = mController.getCurrentUser().getTDEE();
-		int consumedCalories = 0;
-		for (FoodDiaryEntry e : entries)
-			consumedCalories += e.getMealCalories();
-		calorieGoalTF.setText(Integer.toString(tdee));
-		calorieConsumedTF.setText(Integer.toString(consumedCalories));
-		calorieRemainingTF.setText(Integer.toString(tdee - consumedCalories));
-	}
+	public void initialize() {
+        entries = mController.getAllFoodDiaryEntries();
+        breakfastTableView.setItems(entries.filtered(e -> e.getCategory() == Category.Breakfast));
+        lunchTableView.setItems(entries.filtered(e -> e.getCategory() == Category.Lunch));
+        dinnerTableView.setItems(entries.filtered(e -> e.getCategory() == Category.Dinner));
+        snacksTableView.setItems(entries.filtered(e -> e.getCategory() == Category.Snack));
+
+        ObservableList<PieChart.Data> data = FXCollections.observableArrayList();
+        data.addAll(new PieChart.Data("Protein", 0.0), new PieChart.Data("Fat", 0.0),
+                new PieChart.Data("Carbs", 0.0));
+        macroPieChart.setData(data);
+        entries.forEach(e -> updateMacros(e));
+
+        entries.addListener(new ListChangeListener<FoodDiaryEntry>() {
+
+            @Override
+            public void onChanged(Change<? extends FoodDiaryEntry> c) {
+                c.next();
+                if (c.wasAdded()) {
+                    FoodDiaryEntry entry = c.getAddedSubList().get(0);
+                    updateCalorieCounters(entry.getMealCalories());
+                    updateMacros(entry);
+                }
+            }
+        });
+
+        int tdee = mController.getCurrentUser().getTDEE();
+        int consumedCalories = 0;
+        for (FoodDiaryEntry e : entries)
+            consumedCalories += e.getMealCalories();
+        calorieGoalTF.setText(Integer.toString(tdee));
+        calorieConsumedTF.setText(Integer.toString(consumedCalories));
+        calorieRemainingTF.setText(Integer.toString(tdee - consumedCalories));
+    }
+
+    protected void updateMacros(FoodDiaryEntry entry) {
+        macroPieChart.getData().forEach(e -> {
+            String name = e.getName();
+            DoubleProperty value;
+            if (name.equals("Protein")) {
+                value = e.pieValueProperty();
+                value.set(value.get() + entry.getMealProtein());
+            }
+            else if (name.equals("Fat")) {
+                value = e.pieValueProperty();
+                value.set(value.get() + entry.getMealFat());
+            }
+            else if (name.equals("Carbs")) {
+                value = e.pieValueProperty();
+                value.set(value.get() + entry.getMealCarbs());
+            }
+        });
+    }
 
 	protected void updateCalorieCounters(int mealCalories) {
 		int currentConsumed = Integer.parseInt(calorieConsumedTF.getText());
 		int caloriesRemaining = Integer.parseInt(calorieRemainingTF.getText());
 		calorieConsumedTF.setText(Integer.toString(currentConsumed + mealCalories));
-		calorieRemainingTF.setText(Integer.toString(caloriesRemaining - mealCalories));		
+		calorieRemainingTF.setText(Integer.toString(caloriesRemaining - mealCalories));
 	}
 
 	public FoodDiary() {
@@ -112,22 +141,22 @@ public class FoodDiary extends VBox implements SceneNavigation {
 		try {
 		Stage stage = new Stage();
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("addMealForm.fxml"));		
+		loader.setLocation(getClass().getResource("addMealForm.fxml"));
 		Pane pane = loader.load();
-				
+
 		AddMealForm form = loader.getController();
 		stage.setScene(new Scene(pane));
 		stage.initStyle(StageStyle.UTILITY);
 		stage.initModality(Modality.APPLICATION_MODAL);
 		stage.setResizable(false);
 		stage.showAndWait();
-		
+
 		FoodDiaryEntry entry = form.getEntry();
-		
-		if (entry != null) {			
+
+		if (entry != null) {
 			switch (entry.getCategory()) {
 			case Breakfast:
-				((FilteredList) breakfastTableView.getItems()).getSource().add(entry);				
+				((FilteredList) breakfastTableView.getItems()).getSource().add(entry);
 				break;
 			case Lunch:
 				((FilteredList) lunchTableView.getItems()).getSource().add(entry);
@@ -140,9 +169,9 @@ public class FoodDiary extends VBox implements SceneNavigation {
 				break;
 			}
 			mController.addFoodDiaryEntry(entry);
-			
+
 		}
-		
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
